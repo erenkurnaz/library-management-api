a487a59df1dd654285953ab449e2bd958c720764
Eren Kurnaz <h.erenkurnaz@icloud.com>
2024-10-26 20:10:50 +0300

    chore: add redis to docker-compose update environments

--- src/config/config-validation.ts
+++ src/config/config-validation.ts
@@ -2,12 +2,19 @@ import * as Joi from 'joi';
 import { RuntimeMode } from './types/config';
 
 export const configValidation = Joi.object({
-  MODE: Joi.string()
-    .valid(RuntimeMode.DEVELOPMENT, RuntimeMode.PRODUCTION, RuntimeMode.TEST),
+  MODE: Joi.string().valid(
+    RuntimeMode.DEVELOPMENT,
+    RuntimeMode.PRODUCTION,
+    RuntimeMode.TEST,
+  ),
   PORT: Joi.number().required(),
   POSTGRES_DB: Joi.string().required(),
   POSTGRES_HOST: Joi.string().required(),
   POSTGRES_USER: Joi.string().required(),
   POSTGRES_PASSWORD: Joi.string().required(),
+  RESULT_CACHE_TTL: Joi.number().required(),
+  REDIS_HOST: Joi.string().required(),
+  REDIS_PORT: Joi.number().required(),
+  REDIS_PASSWORD: Joi.string().optional(),
   JWT_SECRET: Joi.string().required(),
 });
